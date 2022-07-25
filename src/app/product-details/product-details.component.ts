93aad5332132df2a99301f4d35355270e4c02e02
adrianaghilusi <adrianaghilusi@yahoo.com>
2022-07-25 12:54:44 +0300

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,7 +8,8 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  constructor() {}
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
 }
